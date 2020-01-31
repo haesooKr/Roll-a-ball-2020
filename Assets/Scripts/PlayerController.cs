2e37271d4c5c90ee1c5a780cb1e86f357c5a3943
haesooKr <gotn676@gmail.com>
2020-01-31 00:12:33 -0500

    Added winText variable to script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
     private int count;
@@ -17,6 +18,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetCountText();
+        winText.text = "";
     }
     void FixedUpdate ()
     {
@@ -39,5 +41,8 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
+        if(count >= 12){
+            winText.text = "You Win!";
+        }
     }
 }
